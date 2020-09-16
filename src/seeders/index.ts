475388fdbb51363ddada3e504641599dcae27344
Sonu <sonu@chapter247.com>
2020-09-16 17:07:33 +0530

    removed unsed codes

--- src/seeders/index.ts
+++ src/seeders/index.ts
@@ -1,15 +1,12 @@
-import { Companies, Trips, Users } from "@server/entities";
-import dbConnection from "@server/utills/db.connection";
-import { getRepository } from "typeorm";
 import { default as SeedCompaies } from "./company.seed";
 import { default as SeedFlights } from "./flights.seed";
 import { default as SeedTrips } from "./trips.seed";
 import { default as SeedUsers } from "./users.seed";
 
 export default async () => {
-  // await SeedUsers();
-  // await SeedCompaies();
-  // await SeedTrips();
+  await SeedUsers();
+  await SeedCompaies();
+  await SeedTrips();
   await SeedFlights();
   console.log(`Database seeded successfully!`);
 };
